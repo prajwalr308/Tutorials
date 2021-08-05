21b34463b41bd778be04dbc281c5ed48b45bcff1
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-08-05 20:03:30 +0530

    useEffects use of array dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+
+export default function App() {
+  const [isadded, setIsadded] = useState(false);
+  const [isState, setIsState] = useState(false);
+  useEffect(() => {
+    //yout fucntions like fetch
+    setIsState(!isState);
+    console.log("add method called");
+  }, [isadded]);
+
+  
+  function add() {
+    //your functions
+    setIsadded(!isadded);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={add}> add</button>
+      <div>{`${isState}`}</div>
     </div>
   );
 }
-
-export default App;
